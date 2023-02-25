81474f0344f863d9ca018723acc92a952382e91a
Sidney Sadel <sidneysadel@gmail.com>
2023-02-24 23:52:49 -0500

    Initial Commit

---
+++ sid portfolio/sid_portfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  sid_portfolioApp.swift
+//  sid portfolio
+//
+//  Created by Sidney Sadel on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct sid_portfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
